3561eea40e2441ff555f996f2474f681c7633069
jp_rossi <jp_rossi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-08-12 09:46:02 +0000

    t
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rich@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onAttach <- function(libname, pkgname){
-  pkg.version <- packageDescription("rich", fields = "Version")
-  startup.txt <- paste("\nrich version ", pkg.version, " is loaded\n", "You can access the package vignette by typing vignette('rich_introduction') in the R console", sep="")
-  packageStartupMessage(startup.txt)
-}
-
